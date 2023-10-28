1a3c9848b57cadadaa5d5558681b553ae9524924
murosyo <murozuka.shota@mikilab.doshisha.ac.jp>
2023-10-28 20:17:24 +0900

    共有用に一旦コミット

--- templates/index.js
+++ templates/index.js
@@ -25,3 +25,13 @@ function countChecked() {
         }
     });
 }
+
+const ham = document.querySelector('#js-hamburger');
+const nav = document.querySelector('#js-nav');
+
+ham.addEventListener('click', function () {
+
+    ham.classList.toggle('active');
+    nav.classList.toggle('active');
+
+});
